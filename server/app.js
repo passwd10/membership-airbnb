78dac6df26ec9f7aa18a7bf2b565952b9acf1c35
passwd10 <parkinseo94@naver.com>
2020-08-31 19:24:48 +0900

    Create login, signup pages

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
+const signupRouter = require('./routes/signup');
 
 const app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
