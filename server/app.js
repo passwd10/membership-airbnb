3b9d0258ccf3572c8f3bf1c19ba36033020dc75a
passwd10 <parkinseo94@naver.com>
2020-08-31 19:53:32 +0900

    Remove annotations

--- server/app.js
+++ server/app.js
@@ -11,7 +11,6 @@ const signupRouter = require('./routes/signup');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -26,18 +25,14 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/signup', signupRouter);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
