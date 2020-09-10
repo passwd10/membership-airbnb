29ef4bda0ff352e6293d453d3b017272545681b6
passwd10 <parkinseo94@naver.com>
2020-09-10 16:35:04 +0900

    조건에 맞게 검색이되게 수정해라

--- services/searchService.js
+++ services/searchService.js
@@ -2,12 +2,17 @@ const fs = require('fs');
 
 const filePath = `${__dirname}/../mocks/rooms.json`;
 
-const searchRooms = (place) => {
+const searchRooms = (place, personnel) => {
   const rooms = fs.readFileSync(filePath, 'utf8', (err) => {
     if (err) { throw err; }
   });
 
-  return JSON.parse(rooms).filter(room => room.place === place);
+  return JSON.parse(rooms).filter(room => {
+    const maxPersonnel = room.description1.split(' · ')[0].match(/\d/g)[0];
+    if (Number(personnel) <= Number(maxPersonnel) && room.place === place) {
+      return room;
+    }
+  });
 };
 
 module.exports = { searchRooms };
